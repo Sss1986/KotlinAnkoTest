8049bc0a18745b00c79a42cd74ca781c96e3b71b
Fang Xia <sfxly457762@163.com>
2017-06-12 12:49:42 +0800

    kotlin

---
+++ app/src/main/java/com/xintiaotime/kotlinankotest/Main2Activity.kt
@@ -0,0 +1,15 @@
+package com.xintiaotime.kotlinankotest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+
+
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
